febb84eb8ce8b4e67001b4f8132db9887e6b6712
EnDeRDEVIL <enderdemon31@outlook.com>
2024-03-14 19:28:45 -0300

    commit 14_03 3.2

--- adm/inc/header.inc.php
+++ adm/inc/header.inc.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FTW</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 
 <body>
