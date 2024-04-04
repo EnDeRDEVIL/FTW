67f55f2417716700e26650f9b73e2bc30f39b844
EnDeRDEVIL <enderdemon31@outlook.com>
2024-04-04 20:01:56 -0300

    commit final

--- adm/inc/footer.inc.php
+++ adm/inc/footer.inc.php
@@ -1,5 +1,14 @@
 <footer>
-        <h1>Aqui será meu rodapé</h1>
+        
+        <div class="container">
+                <ul class="social">
+                        <a href="https://www.facebook.com"><li>Facebook</li></a>
+                        <a href="https://www.twitter.com"><li>Twitter</li></a>
+                        <a href="https://www.instagram.com"><li>Instagram</li></a>
+                </ul>
+                <hr>
+                <h4>&copy Todos os direitos reservados</h4>
+        </div>
 </footer>
 
 <script rel="text/javascript" src="js/jquery-3.7.1.min.js"></script>
--- adm/inc/header.inc.php
+++ adm/inc/header.inc.php
@@ -5,15 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
     <title>FTW</title>
 </head>
 
 <body>
-    <div class="fluid-container bg-danger text-center">
+    <div class="fluid-container">
     <header>
         <div class="navbar navbar-warning bg-warning">
             <div class="fluid-container d-flex justify-content-between">
-                <img src="img/logo2.png" class="img-fluid" style="width: 20%">
+                <a href="index.php"><img src="img/logo2.png" class="img-fluid" style="width: 20%"></a>
+                <link rel="stylesheet" type="text/css" href="css/style.css">
             </div>
 
             <nav>
